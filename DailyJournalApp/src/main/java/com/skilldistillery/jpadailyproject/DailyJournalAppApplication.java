35f2ba97298134b48b2e21f4914623e82af99837
Mason Keeney <mason.keeney68@gmail.com>
2022-05-13 16:50:27 -0600

    Spring Boot DailyJournalApp added and connected to JPADailyProject

---
+++ DailyJournalApp/src/main/java/com/skilldistillery/jpadailyproject/DailyJournalAppApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpadailyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyJournalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyJournalAppApplication.class, args);
+	}
+
+}
---
+++ DailyJournalApp/src/test/java/com/skilldistillery/jpadailyproject/DailyJournalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpadailyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyJournalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
